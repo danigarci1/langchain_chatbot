d13c118213f3460e80fddd3f7d1e0bd69dd1f59b
Daniel García Peña <dgarcia@dsiplus.es>
2024-08-18 09:43:59 +0200

    Session moved to different file

--- app/server.py
+++ app/server.py
@@ -8,20 +8,18 @@ but will help us to demonstrate the RunnableWithMessageHistory interface.
 We'll use cookies to identify the user. This will help illustrate how to
 fetch configuration from the request.
 """
-import re
-from pathlib import Path
 from typing import Any, Callable, Dict, Union
 
 from fastapi import FastAPI, HTTPException, Request
-from langchain_community.chat_message_histories import FileChatMessageHistory
 from langchain_core import __version__
-from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables import ConfigurableFieldSpec
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langserve import add_routes
 from typing_extensions import TypedDict
 
 from app.chain import get_chain
+from app.session import create_session_factory
+
 
 # Define the minimum required version as (0, 1, 0)
 # Earlier versions did not allow specifying custom config fields in
@@ -38,55 +36,6 @@ if LANGCHAIN_CORE_VERSION < MIN_VERSION_LANGCHAIN_CORE:
     )
 
 
-def _is_valid_identifier(value: str) -> bool:
-    """Check if the value is a valid identifier."""
-    # Use a regular expression to match the allowed characters
-    valid_characters = re.compile(r"^[a-zA-Z0-9-_]+$")
-    return bool(valid_characters.match(value))
-
-
-def create_session_factory(
-    base_dir: Union[str, Path],
-) -> Callable[[str, str], BaseChatMessageHistory]:
-    """Create a factory that can retrieve chat histories.
-
-    The chat histories are keyed by user ID and conversation ID.
-
-    Args:
-        base_dir: Base directory to use for storing the chat histories.
-
-    Returns:
-        A factory that can retrieve chat histories keyed by user ID and conversation ID.
-    """
-    base_dir_ = Path(base_dir) if isinstance(base_dir, str) else base_dir
-    if not base_dir_.exists():
-        base_dir_.mkdir(parents=True)
-
-    def get_chat_history(user_id: str, conversation_id: str) -> FileChatMessageHistory:
-        """Get a chat history from a user id and conversation id."""
-        if not _is_valid_identifier(user_id):
-            raise ValueError(
-                f"User ID {user_id} is not in a valid format. "
-                "User ID must only contain alphanumeric characters, "
-                "hyphens, and underscores."
-                "Please include a valid cookie in the request headers called 'user-id'."
-            )
-        if not _is_valid_identifier(conversation_id):
-            raise ValueError(
-                f"Conversation ID {conversation_id} is not in a valid format. "
-                "Conversation ID must only contain alphanumeric characters, "
-                "hyphens, and underscores. Please provide a valid conversation id "
-                "via config. For example, "
-                "chain.invoke(.., {'configurable': {'conversation_id': '123'}})"
-            )
-
-        user_dir = base_dir_ / user_id
-        if not user_dir.exists():
-            user_dir.mkdir(parents=True)
-        file_path = user_dir / f"{conversation_id}.json"
-        return FileChatMessageHistory(str(file_path))
-
-    return get_chat_history
 
 
 app = FastAPI(
@@ -155,15 +104,9 @@ add_routes(
     app,
     chain_with_history,
     per_req_config_modifier=_per_request_config_modifier,
-    # Disable playground and batch
-    # 1) Playground we're passing information via headers, which is not supported via
-    #    the playground right now.
-    # 2) Disable batch to avoid users being confused. Batch will work fine
-    #    as long as users invoke it with multiple configs appropriately, but
-    #    without validation users are likely going to forget to do that.
-    #    In addition, there's likely little sense in support batch for a chatbot.
     disabled_endpoints=["playground", "batch"],
 )
+
 
 if __name__ == "__main__":
     import uvicorn
