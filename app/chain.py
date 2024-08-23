35a040d194bba361b7543f73e37ea1057c39b3e8
Daniel García Peña <dgarcia@dsiplus.es>
2024-08-23 10:18:53 +0200

    added prompt

--- app/chain.py
+++ app/chain.py
@@ -2,11 +2,16 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableSerializable
 
+def load_prompt(filename):
+    with open(filename, 'r') as file:
+        return file.read()
+
+prompt = load_prompt('.prompt')
 
 def get_chain() -> RunnableSerializable:
     prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", "You're an assistant by the name of Bob."),
+            ("system", prompt),
             MessagesPlaceholder(variable_name="history"),
             ("human", "{human_input}"),
         ]
